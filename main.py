c04485645803991df2799e3695368d37de0d9285
akarshanghawri <akarshanghawri@gmail.com>
2026-02-11 19:27:18 +0530

    Improve index page UI and update routes

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+from .models import User,Workout
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
+@login_required
 def index() :
-    return render_template("index.html")
+    workouts = Workout.query.filter_by(user_id=current_user.id).all()
+    return render_template("index.html",workouts=workouts)
+
+@main.route('/add')
+@login_required
+def add_workout() :
+    return
+
+@main.route('/edit/<int:id>')
+@login_required
+def edit_workout(id) :
+    return
+
+@main.route('/delete/<int:id>')
+@login_required
+def delete_workout(id) :
+    return
 
 @main.route('/profile')
 @login_required
